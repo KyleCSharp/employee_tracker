62b5b318c9a1819e2dd2688d4808ab258a25c02d
Kylecsharp <kylelrigler@gmail.com>
2023-03-04 20:06:56 -0500

    fixed DB seed names vs insert

--- employee_tracker.db/dbo/Tables/employee.sql
+++ employee_tracker.db/dbo/Tables/employee.sql
@@ -3,12 +3,12 @@
     [ID]       INT           NOT NULL,
     [Email]    VARCHAR (100) NULL,
     [phone]    VARCHAR (20)  NULL,
-    [Address 1]VARCHAR (100) NULL,
-	[Address 2]VARCHAR (100) NULL,
+    [Address_1]VARCHAR (100) NULL,
+	[Address_2]VARCHAR (100) NULL,
 	[City]		VARCHAR(50)	NULL,
 	[State]		VARCHAR(100)NULL,
 	[Zip_Code]	VARCHAR(50) NULL,
-	[Pay Rate] MONEY         NOT NULL,
+	[Pay_Rate] MONEY         NOT NULL,
     [Hired_at] DATETIME      NOT NULL
 
 );
